2c731ecae1d8b980ef2ac107d7f4dbf33498f949
Mallory <meowry.koai@gmail.com>
2015-07-30 23:27:47 -0500

    Add largestPrime method.
    Need to handle large integers.

--- Problem3.java
+++ Problem3.java
@@ -1,9 +1,22 @@
+import java.math.BigInteger;
+
 /**
  * Created by Mirenze on 7/29/2015.
  */
 public class Problem3 {
     public static void main(String[] args) {
-        System.out.println(isPrime(99));
+
+        BigInteger target = new BigInteger("600851475143");
+        System.out.println(largestPrime(target));
+    }
+
+    public static int largestPrime(int num) {
+        for (int i = num; i > 1; i--) {
+            if (num % i == 0 && isPrime(i)) {
+                return i;
+            }
+        }
+        return 0;
     }
 
     public static boolean isPrime(int num) {
