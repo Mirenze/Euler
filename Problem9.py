1e5c76ab0058a272c416f8156a873e4f7503e7f8
Mallory <meowry.koai@gmail.com>
2015-08-09 00:56:07 -0500

    Add solution for problem 9.
    Use built-in sum() function.

--- Problem9.py
+++ Problem9.py
@@ -2,6 +2,7 @@ __author__ = 'Mirenze'
 
 from functools import reduce
 
+# Euclid's formula for generating Pythagorean triples
 def generateTriple(m, n) :
     triple = {}
     triple['a'] = m**2 - n**2
@@ -9,8 +10,18 @@ def generateTriple(m, n) :
     triple['c'] = m**2 + n**2
     return triple
 
-def sumTriple(triple) :
-    return sum(triple.values(), 0)
-
-def productTriple(triple) :
+def product(triple) :
     return reduce(lambda x, value : x * value, triple.values(), 1)
+
+""" Since the sum of a, b, and c cannot exceed 1000, it follows that
+ c = m^2 + n^2 also cannot exceed 1000. Therefore, the greatest possible
+ value for m in the context of variable c is 31. Temporary heuristic
+ that can definitely be fine-tuned later. """
+def problem9() :
+    for n in range(1, 31) :
+        for m in range(n + 1, 32) :
+            triple = generateTriple(m, n)
+            if sum(triple.values()) == 1000 :
+                return product(triple)
+
+print(problem9())
